736f83277c2b1dbc754029c7af124c3da0b9dcb5
diaba <hwar@cincinnatistate.edu>
2022-01-13 09:45:28 -0500

    generate my components

--- todoClient/src/app/app.module.ts
+++ todoClient/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CategoryComponent } from './category/category.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CategoryComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
